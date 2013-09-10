b8e7ed4606adff70119246ae67dc370a918d11d2
Ethan Furman <ethan@stoneleaf.us>
2013-09-10 10:01:01 -0700

    Now propertly handles module names.

--- xid.py
+++ xid.py
@@ -1,15 +1,25 @@
 "various OpenERP routines"
 
-def get_xml_ids(obj, cr, uid, ids, name=None, arg=None, context=None):
+from fnx import check_company_settings
+
+
+def get_xml_ids(obj, cr, uid, ids, name=None, fld_desc_err=None, context=None):
+    """able to handle multiple records at once"""
+    if context is None:
+        context = {}
+    if fld_desc_err is None:
+        raise ValueError('FIS Integration setting must be specified')
+    field, desc, error = fld_desc_err
+    settings = check_company_settings(obj, cr, uid, fld_desc_err)
+    module = settings[field]
+    model = obj._name
     imd = obj.pool.get('ir.model.data')
     result = {}
     for id in ids:
         try:
-            res = imd.get_object_from_model_resid(
-                    cr,
-                    uid,
-                    obj._name,
-                    id,
+            res = imd.get_object_from_module_model_resid(
+                    cr, uid,
+                    module, model, id,
                     context=context,
                     )
         except ValueError:
@@ -18,8 +28,26 @@ def get_xml_ids(obj, cr, uid, ids, name=None, arg=None, context=None):
             result[id] = res['name']
     return result
 
+def update_xml_id(obj, cr, uid, id, field_name, field_value, fld_desc_err, context=None):
+    """one record at a time"""
+    if context is None:
+        context = {}
+    if fld_desc_err is None:
+        raise ValueError('FIS Integration setting must be specified')
+    field, desc, error = fld_desc_err
+    settings = check_company_settings(obj, cr, uid, fld_desc_err)
+    module = settings[field]
+    model = obj._name
+    imd = obj.pool.get('ir.model.data')
+    try:
+        rec = imd.get_object_from_module_model_resid(cr, uid, module, model, id, context=context)
+    except ValueError:
+        imd.create(cr, uid, {'module':module, 'model':model, 'res_id':id, 'name':field_value, 'noupdate':True}, context=context)
+    else:
+        imd.write(cr, uid, rec.id, {'module':module, 'model':model, 'res_id':id, 'name':field_value, 'noupdate':True}, context=context)
+    return True
 
-def search_xml_id(obj, cr, uid, model, name, domain, context=None):
+def search_xml_id(obj, cr, uid, module, name, domain, fld_desc_err=None, context=None):
     """
     domain[0][0] = 'xml_id'
     domain[0][1] = 'ilike', 'not ilike', '=', '!='
@@ -29,32 +57,25 @@ def search_xml_id(obj, cr, uid, model, name, domain, context=None):
         return []
     if not context:
         context = {}
+    if fld_desc_err is None:
+        raise ValueError('FIS Integration setting must be specified')
+    field, desc, error = fld_desc_err
+    settings = check_company_settings(obj, cr, uid, fld_desc_err)
+    module = settings[field]
     imd = obj.pool.get('ir.model.data')
     records = imd._get_model_records(cr, uid, obj._name)
     (field, op, text) ,= domain
     itext = text.lower()
     if op == 'ilike':
-        id_names = [(r['res_id'], r['name']) for r in records if itext in r['name'].lower()]
+        id_names = [(r.res_id, r.name) for r in records if itext in r.name.lower() and r.module == module]
     elif op == 'not ilike':
-        id_names = [(r['res_id'], r['name']) for r in records if itext not in r['name'].lower()]
+        id_names = [(r.res_id, r.name) for r in records if itext not in r.name.lower() and r.module == module]
     elif op == '=':
-        id_names = [(r['res_id'], r['name']) for r in records if text == r['name']]
+        id_names = [(r.res_id, r.name) for r in records if text == r.name and r.module == module]
     elif op == '!=':
-        id_names = [(r['res_id'], r['name']) for r in records if text != r['name']]
+        id_names = [(r.res_id, r.name) for r in records if text != r.name and r.module == module]
     else:
         raise ValueError('invalid op for external_id: %s' % op)
     return [('id', 'in', [x[0] for x in id_names])]
 
 
-def update_xml_id(obj, cr, uid, id, field, value, model, context=None):
-    imd = obj.pool.get('ir.model.data')
-    if context is None:
-        context = {}
-    module = context.get('module', '')
-    try:
-        rec = imd.get_object_from_model_resid(cr, uid, model, id, context=context)
-    except ValueError:
-        imd.create(cr, uid, {'module':module, 'name':value, 'model':model, 'res_id':id, 'noupdate':True}, context=context)
-    else:
-        imd.write(cr, uid, rec['id'], {'module':module, 'name':value, 'model':model, 'res_id':id, 'noupdate':True}, context=context)
-    return True
