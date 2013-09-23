f62f8561f58d340da73207aed60fec091f23197c
Ethan Furman <ethan@stoneleaf.us>
2013-09-23 09:09:25 -0700

    more fixes for the ir_model_data cleanup

--- xid.py
+++ xid.py
@@ -20,28 +20,35 @@ def get_xml_ids(obj, cr, uid, ids, field_names, arg, context=None):
     imd = obj.pool.get('ir.model.data')
     ids = set(ids)
     result = defaultdict(dict)
+    imd_records = {}
     for rec in imd.get_model_records(cr, uid, model):
-        if rec.id in ids:
-            if rec.module == module:
-                result[rec.id]['xml_id'] = rec.name
-                result[rec.id]['module'] = rec.module
-            else:
-                result[rec.id]['xml_id'] = ''
-                result[rec.id]['module'] = ''
-    return result
+        imd_records[rec.res_id] = rec
+    for id in ids:
+        rec = imd_records.get(id)
+        if rec is not None and rec.module == module:
+                result[id]['xml_id'] = rec.name
+                result[id]['module'] = rec.module
+        else:
+            result[id]['xml_id'] = ''
+            result[id]['module'] = ''
+    return dict(result)
 
-def update_xml_id(obj, cr, uid, id, module, xml_id, context=None):
+def update_xml_id(obj, cr, uid, id, field_name, field_value, arg, context=None):
     """one record at a time"""
     if context is None:
         context = {}
+    if field_name == 'module':
+        return True
     model = obj._name
+    settings = check_company_settings(obj, cr, uid, arg)
+    module = settings[arg[0]]
     imd = obj.pool.get('ir.model.data')
     try:
-        rec = imd.get_object_from_model_resid(cr, uid, module, model, id, context=context)
+        rec = imd.get_object_from_model_resid(cr, uid, model, id, context=context)
     except ValueError:
-        imd.create(cr, uid, {'model':model, 'res_id':id, 'module':module, 'name':xml_id}, context=context)
+        imd.create(cr, uid, {'model':model, 'res_id':id, 'module':module, 'name':field_value}, context=context)
     else:
-        imd.write(cr, uid, rec.id, {'model':model, 'res_id':id, 'module':module, 'name':xml_id}, context=context)
+        imd.write(cr, uid, rec.id, {'model':model, 'res_id':id, 'module':module, 'name':field_value}, context=context)
     return True
 
 def search_xml_id(obj, cr, uid, obj_again, field_name, domain, context=None):
@@ -52,19 +59,21 @@ def search_xml_id(obj, cr, uid, obj_again, field_name, domain, context=None):
     """
     if not len(domain):
         return []
+    if field_name == 'xml_id':
+        field_name = 'name'
     imd = obj.pool.get('ir.model.data')
     model = obj._name
     records = imd.get_model_records(cr, uid, model)
     (field, op, text) ,= domain
     itext = text.lower()
     if op == 'ilike':
-        id_names = [(r.res_id, r.name) for r in records if itext in r.name.lower()]
+        id_names = [(r.res_id, r[field_name]) for r in records if itext in r[field_name].lower()]
     elif op == 'not ilike':
-        id_names = [(r.res_id, r.name) for r in records if itext not in r.name.lower()]
+        id_names = [(r.res_id, r[field_name]) for r in records if itext not in r[field_name].lower()]
     elif op == '=':
-        id_names = [(r.res_id, r.name) for r in records if text == r.name]
+        id_names = [(r.res_id, r[field_name]) for r in records if text == r[field_name]]
     elif op == '!=':
-        id_names = [(r.res_id, r.name) for r in records if text != r.name]
+        id_names = [(r.res_id, r[field_name]) for r in records if text != r[field_name]]
     else:
         raise ValueError('invalid op for external_id: %s' % op)
     return [('id', 'in', [x[0] for x in id_names])]
