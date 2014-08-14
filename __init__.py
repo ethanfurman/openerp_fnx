21f0b665536e8a979ef3d473c9a208b5a3a0d80e
Ethan Furman <ethan@stoneleaf.us>
2014-08-13 18:04:17 -0700

    added static and dynamic html page stubs

--- __init__.py
+++ __init__.py
@@ -97,3 +97,99 @@ def Proposed(obj, values, record=None):
             [(k, record[k]) for k in obj._columns.keys()],
             values,
             )
+
+
+dynamic_page_stub = """\
+<HTML>
+  <HEAD>
+    <script type="text/javascript">
+
+/**********************************************************************************
+* Base on code from
+*
+* Dynamic Ajax Content- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+**********************************************************************************/
+
+
+var bustcachevar = 1 //bust potential caching of external pages after initial request? (1=yes, 0=no)
+var loadedobjects = ""
+var rootdomain = "http://"+window.location.hostname
+var bustcacheparameter = ""
+
+function ajaxpage(url, containerid)
+    {
+    var xmlhttp = false;
+    if (window.XMLHttpRequest) // IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    else
+        return false;
+    xmlhttp.onreadystatechange = function()
+        {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+            loadpage(xmlhttp, containerid);
+        }
+    if (bustcachevar) //if bust caching of external page
+        bustcacheparameter = (url.indexOf("?")!=-1) ? "&"+new Date().getTime() : "?"+new Date().getTime();
+    xmlhttp.open('GET', url+bustcacheparameter, true);
+    xmlhttp.send();
+    }
+
+function loadpage(xmlhttp, containerid)
+    {
+    //if (xmlhttp.readyState == 4 && (xmlhttp.status==200 || window.location.href.indexOf("http")==-1))
+    //alert("alerting..."+xmlhttp+"...alerted")
+    document.getElementById(containerid).innerHTML = xmlhttp.responseText
+    }
+
+function loadobjs()
+    {
+    if (!document.getElementById)
+        return;
+    for (i=0; i<arguments.length; i++)
+        {
+        var file = arguments[i];
+        var fileref = "";
+        if (loadedobjects.indexOf(file) == -1) //Check to see if this object has not already been added to page before proceeding
+            {
+            if (file.indexOf(".js") != -1) //If object is a js file
+                {
+                fileref = document.createElement('script');
+                fileref.setAttribute("type","text/javascript");
+                fileref.setAttribute("src", file);
+                }
+            else if (file.indexOf(".css") != -1) //If object is a css file
+                {
+                fileref = document.createElement("link");
+                fileref.setAttribute("rel", "stylesheet");
+                fileref.setAttribute("type", "text/css");
+                fileref.setAttribute("href", file);
+                }
+            }
+        if (fileref != "")
+            {
+            document.getElementsByTagName("head").item(0).appendChild(fileref);
+            loadedobjects += file + " "; //Remember this object as being already added to page
+            }
+        }
+    }
+
+    </script>
+  </HEAD>
+  <BODY>
+      %s
+  </BODY>
+</HTML>
+"""
+
+static_page_stub = """\
+<HTML>
+  <HEAD>
+  </HEAD>
+  <BODY>
+      %s
+  </BODY>
+</HTML>
+"""
+
