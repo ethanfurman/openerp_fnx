d58b4f72545a991164d879ac4b55a19ee10c3801
Ethan Furman <ethan@stoneleaf.us>
2016-04-21 15:58:14 -0700

    convert humanize to Humanize, a browse_record wrapper

--- __init__.py
+++ __init__.py
@@ -10,6 +10,9 @@ from VSS import address, dbf, enum, finance, path, utils, BBxXlate, time_machine
 from VSS.address import *
 from VSS.time_machine import PropertyDict
 from VSS.utils import *
+import logging
+
+_logger = logging.getLogger()
 
 # make dbf and path look like submodules of fnx so other modules can do `from fnx.path import Path'
 sys.modules['fnx.address'] = address
@@ -63,64 +66,92 @@ class SortedDocEnum(enum.Enum):
                 namespace[name] = member
     export_to = export
 
-def humanize(browse_record, context=None, _seen=None):
-    'return a PropertyDict with all fields converted to text'
-    context = context or browse_record._context or {}
-    cr = browse_record._cr
-    uid = browse_record._uid
-    model = browse_record._model
-    field_names = model._columns
-    seen = _seen or {}
-    key = model, browse_record.id
-    if key in seen:
-        return seen[key]
-    record = PropertyDict()
-    seen[key] = record
-    for field_name in field_names:
-        field = model._columns[field_name]
-        value = browse_record[field_name]
-        if field._type in ('many2one', 'one2many', 'many2many'):
-            record[field_name] = value
-        elif field._type in ('reference', ):
+
+class Humanize(object):
+    'a browse_record wrapper that converts fields to text'
+
+    def __init__(self, browse_record, context={}):
+        self.context = browse_record._context
+        self.context.update(context)
+        self.browse_record = browse_record
+
+    def __getattr__(self, name):
+        record = self.browse_record
+        columns = dict([(k, v[2]) for k, v in record._model._inherit_fields.items()])
+        columns.update(record._model._columns)
+        value = getattr(record, name)
+        if name == 'name_get':
+            try:
+                return record.name_get()[0][1]
+            except Exception, exc:
+                return unicode(record[model._rec_name])
+                _logger.exception('problem with name_get (%s.%s); using %s' % (model._table, name, result))
+        elif name not in columns:
+            return value
+        cr = record._cr
+        uid = record._uid
+        model = record._model
+        field = columns[name]
+        type = field._type
+        cls = self.__class__
+        if type in ('many2one', ):
+            if value:
+                result = cls(value, context=self.context)
+        elif type in ('one2many', 'many2many'):
+            result = [cls(v, context=self.context) for v in value]
+        elif type in ('reference', ):
             if value:
                 subrecord = value
-                value = subrecord.name_get()[0]
-            record[field_name] = value or ''
-        elif field._type in ('boolean', ):
+                value = subrecord.name_get()[0][1]
+            result = value or ''
+        elif type in ('boolean', ):
             choice = field.choice
-            record[field_name] = choice[value]
-        elif field._type in ('integer', 'float'):
-            record[field_name] = value or 0
-        elif field._type in ('float', ):
-            record[field_name] = value or 0.0
-        elif field._type in ('html', 'text', 'char', 'date', 'binaryname', 'serialized'):
-            record[field_name] = value or ''
-        elif field._type in ('datetime', ):
+            result = choice[value]
+        elif type in ('integer', ):
+            result = value or 0
+        elif type in ('float', ):
+            result = value or 0.0
+        elif type in ('html', 'text', 'char', 'binaryname', 'serialized'):
+            result = value or ''
+        elif type in ('datetime', ):
             if value:
-                value = field.context_timestamp(cr, uid, value, context)
+                value = field.context_timestamp(cr, uid, value, self.context)
                 value = value.strftime('%Y-%m-%d %H:%M')
-                record[field_name] = value
+                result = value
             else:
-                record[field_name] = ''
-        elif field._type in ('binary', ):
-            record[field_name] = '%d bytes' % len(value)
-        elif field._type in ('selection', ):
+                result = ''
+        elif type in ('date', ):
+            if value:
+                result = value
+            else:
+                result = ''
+        elif type in ('binary', ):
+            result = '%d bytes' % len(value or '')
+        elif type in ('selection', ):
             if not value:
-                record[field_name] = ''
-                continue
-            elif isinstance(field.selection, (tuple, list)):
+                value = ''
+            if isinstance(field.selection, (tuple, list)):
                 selection = field.selection
             else:
                 # call the 'dynamic selection' function
-                selection = field.selection(model, cr, uid, context)
+                selection = field.selection(model, cr, uid, self.context)
             for internal, user in selection:
                 if internal == value:
-                    value = user
+                    result = user
                     break
             else:
-                value = '<invalid internal value: %r>' % value
-            record[field_name] = value
+                if value:
+                    result = '<invalid internal value: %r>' % value
+                else:
+                    result = value
         else:
-            record[field_name] = '<unk>'
-    return record
+            value = record[name]
+            result = unicode(value) if value else ''
+        return result
+
+    def __getitem__(self, name):
+        try:
+            return getattr(self, name)
+        except AttributeError:
+            raise KeyError('%s not found' % name)
 
